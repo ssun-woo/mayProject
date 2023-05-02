4e49ab2a6be23e279bd935e1b0184cf705f34e39
sw <sw@naver.com>
2023-05-02 12:58:18 +0900

    5월프로젝트

---
+++ src/main/MainClass.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		while(true) {
+			System.out.println("1. 회원 기능");
+			System.out.println("2. 이벤트 기능");
+			System.out.println("3. 구매 기능");
+			System.out.println("4. 게시글 기능");
+			
+			int choice = scan.nextInt();
+			
+			switch(choice) {
+			case 1 :
+				System.out.println("회원 기능");
+				break;
+			case 2 :
+				System.out.println("이벤트 기능");
+				break;
+			case 3 :
+				System.out.println("구매 기능");
+				break;
+			case 4 :
+				System.out.println("게시글 기능");
+				break;
+			default :
+				System.out.println("잘못된 입력입니다");
+				break;
+					
+			}
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
