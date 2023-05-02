ba492484485aaab46fbd436ad9f9e66388a41a9a
seokju <lagreo0607@naver.com>
2023-05-02 13:25:34 +0900

    석주

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.Scanner;
 
+import member.MemberServiceImpl;
 import shopping.ShoppingService;
 import shopping.ShoppingServiceimpl;
 
@@ -11,7 +12,7 @@ public class MainClass {
 
 		Scanner scan = new Scanner(System.in);
 		ShoppingService ss = new ShoppingServiceimpl();
-
+        MemberServiceImpl mb = new MemberServiceImpl();
 		while (true) {
 			System.out.println("1. 회원 기능");
 			System.out.println("2. 이벤트 기능");
@@ -23,6 +24,8 @@ public class MainClass {
 			switch (choice) {
 			case 1:
 				System.out.println("회원 기능");
+				mb.addMember("홍길동", "hgd@naver.com", "1234");
+		
 				break;
 			case 2:
 				System.out.println("이벤트 기능");
--- src/member/MemberServiceImpl.java
+++ src/member/MemberServiceImpl.java
@@ -1,5 +1,24 @@
 package member;
 
-public class MemberServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MemberServiceImpl implements MemberService {
+	    private List<MemberService> members = new ArrayList<>();
+
+	  
+	    public void addMember(String name, String email, String password) {
+	       System.out.println(name+email+password);
+	    }
+
+
+		
+		public void showMemberCount() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		
+		
 }
